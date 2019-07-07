8ed32cb9b31bd26c01635baabe51e04393af4cbe
manish <mandecent.gupta@gmail.com>
2019-07-06 23:23:15 -0400

    fixed logo

--- deps/kb_mfmd/install-mfmd.sh
+++ deps/kb_mfmd/install-mfmd.sh
@@ -2,7 +2,6 @@
 R CMD INSTALL /kb/deps/kb_mfmd/Rserve_1.7-3.1.tar.gz
 Rscript /kb/deps/kb_mfmd/script.R
 git clone https://github.com/jadermcg/mfmd.git
-#pip install weblogo
-#cd mfmd
-#java -jar mfmd.jar dataset_crp.fasta 22 0.005    //system call
+pip install weblogo
+cd mfmd
 cd ..
